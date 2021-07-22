7b9e757f75f3e4435a44a75bd4dba1681aa54a89
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-07-22 11:01:53 +0200

    tailwind install

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ function App() {
   return (
     <div className="App">
       <h1>API</h1>
+
+      <ul className="space-y-2">
+  <li>
+    <ul className="grid grid-cols-10 h-7">
+      <li className="bg-red-50"></li>
+      <li className="bg-red-100"></li>
+      <li className="bg-red-200"></li>
+      <li className="bg-red-300"></li>
+      <li className="bg-red-400"></li>
+      <li className="bg-red-500"></li>
+      <li className="bg-red-600"></li>
+      <li className="bg-red-700"></li>
+      <li className="bg-red-800"></li>
+      <li className="bg-red-900"></li>
+    </ul>
+  </li>
+  </ul>
+
     </div>
   );
 }
